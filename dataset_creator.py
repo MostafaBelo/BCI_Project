eae7a1f8044de5a881574d203024e5375deadf09
Mostafa Elfaggal <mostafaelfaggal@gmail.com>
2025-12-19 11:43:59 +0200

    non localized dataset creator

--- dataset_creator.py
+++ dataset_creator.py
@@ -6,11 +6,12 @@ from tqdm import tqdm
 
 import os
 
-data_dir = "/home/g4/Documents/BCI_Project/Dataset/Task1_Matlab/Matlab files"
+data_dir = "/mnt/C/BCI Dataset/Task1_Matlab/Matlab files"
+# data_dir = "/home/g4/Documents/BCI_Project/Dataset/Task1_Matlab/Matlab files"
 for file in tqdm(os.listdir(data_dir)):
     subject_name = file.strip("results").strip("_SR.mat")
-    if subject_name == "ZAB":
-        continue
+    # if subject_name == "ZAB":
+    #     continue
     print(subject_name)
 
     mat = loadmat(os.path.join(data_dir, file))
@@ -21,7 +22,8 @@ for file in tqdm(os.listdir(data_dir)):
         try:
             localized_eeg = localizer.localize(eeg_signal)
             filter_local_eeg = region_filter.filter_signal(localized_eeg)
-            res[0, i] = filter_local_eeg
+
+            res[0, i] = eeg_signal
         except Exception as e:
             print(f"Error processing sentence {i}: {e}")
             print(eeg_signal.shape)
